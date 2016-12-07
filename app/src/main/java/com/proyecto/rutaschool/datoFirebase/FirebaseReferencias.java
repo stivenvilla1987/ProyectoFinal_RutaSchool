7266ac3e085ad795a911566a4108a68153c3a7e5
jdgallego <jdgallego1029@gmail.com>
2016-12-06 22:12:17 -0500

    COnexion con firebase base de datos

--- app/src/main/java/com/proyecto/rutaschool/datoFirebase/FirebaseReferencias.java
+++ app/src/main/java/com/proyecto/rutaschool/datoFirebase/FirebaseReferencias.java
@@ -8,4 +8,5 @@ public class FirebaseReferencias {
     final public static String RUTA_REFERENCIA = "localizacion";
     final public static String DATOS_REFERENCIA = "datos";
     final public static String USUARIO_REFERENCIA = "usuario";
+    final public static String USUARIOS_REFERENCIA = "usuarios";
 }
